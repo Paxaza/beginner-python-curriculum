ab00a00afc311ae08bccfea555aec31b4fd91c43
Paxaza <annabelle.r.brennan@gmail.com>
2026-09-20 01:10:02 +0000

    input

---
+++ lesson2/classwork/math_operators.py
@@ -0,0 +1,13 @@
+# print(4 + 5) # Addition: adds two numbers
+
+# print(10 - 3) # Subtraction: subtracts the second number from the first number
+
+# print(10 * 7) # Multiplication: multiplies two numbers
+
+# print(15 / 2) # Division: divides and gives float result
+
+# print(15 // 2) # Floor Division: divides and rounds down to teh nearest whole number
+
+# print(15 % 2) # Modulus: gives the remainder
+
+# follows PEMDAS rules
--- lesson2/classwork/practice.py
+++ lesson2/classwork/practice.py
@@ -1,13 +1,25 @@
 # Problem 1
 # Ask the user for their age.
 # Calculate and print how many decades old they are, rounded to the nearest whole number.
-
+age_i = input ("How old are you? ")
+age = int(age_i)
+decades = (age // 10)
+print("Ah so you're ", str(age) + ". That's around", str(decades) + " decades!")
+if decades <=2:
+    print("Be safe. You still have a long life to live.")
+    if decades >=6:
+        print("You've hopefully lived a fufilling life so far. Continue to enjoy the freedom that comes with old age.")
+else:
+    print("Continue living life. No matter what challenges you face. You can do hard things!")
 
 
 # Problem 2
 # Ask the user to enter a number.
 # Print the result of multiplying that number by 5.
-
+imp_number = input ("Now, enter a number. Any number... ")
+nwnum = int(imp_number)
+numfnl = (nwnum * 5)
+print("Ah... your result appears to be", str(numfnl) + ".")
 
 
 # Problem 3
