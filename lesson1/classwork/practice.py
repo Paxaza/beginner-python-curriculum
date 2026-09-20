65479ba1d7cd0cc12a2643a02ee5042feb8837d4
Paxaza <annabelle.r.brennan@gmail.com>
2026-09-20 01:09:10 +0000

    input

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
-
+favorite_number = 4
+print(favorite_number)
 
 # Problem 2
 # Create a variable for your height in meters and print it.
-
+height = 1.6256
+print(height)
 
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
-
+animal = "fox"
+print("My favorite animal is a", animal)
 
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
-
+first = "Annabelle"
+last = "Brennan"
+print(first)
+print(last)
 
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+name = "Annabelle"
+age = 15
+color = "green"
+print("Hello! My name is", name + ". I am", age, "and my favorite color is", color + ".")
